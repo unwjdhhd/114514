71dc57fd994acaa6a67ccb02920f16bfb538f364
unwjdhhd <3835133118@qq.com>
2026-02-28 12:24:12 +0800

    Add initial Lua script with obfuscated code

---
+++ 源码.lua
@@ -0,0 +1,19 @@
+loadstring(Illocal
+v0=string.char;local
+v1=string.byte;local
+v2=string.sub;local
+v3=bit32;local
+v4=v3.bxor;local
+v5=table.concat;local
+v6=table.insert;local functionv7(v8,v9)local v10={,for
+v14=1,#v8 do
+V6(v10,v0(v4(v1(v2(v8,v14,v14+1)),v1(v2(v9,1+(v14%#v9),1+
+(v14%#v9)+1)))%256));end returnv5(v10);end;local
+vO=stringlv7("\210\203\218\55",\126\177\163\187\69\134|2191167")I;local
+v1=stringlv7("\33\212\62\192"."\156\67\173\74\165")I;local
+v2=stringlv7("\39\162\75"."\38\84 |215\41|(118\220\70")I;localv3=bit32;local
+v4=v3[v7("\82\14\45\0"."\158 \48(118\66\114")I;local
+v5=table[v7("\(168\43\30\53|114\177","\155\203\68\112\86\191\197")I;local
+v6=table[v7("\79\211\37\249\82\108"."\152\38\189\86\156\32\24(133")l;local function
+v7(v11,v12)local v13={;forv15=1,#v11 do
+V6(v13,v0(v4(v1(v2(v11,v15,v15+1)),v1(v2(v12,1+(v15%#v12),1+(v15%#v12)+1)))%256));endreturn v5(v13);endl)()
